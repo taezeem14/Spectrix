23c9a13e1ca340f6c64176e073823c1d572fc9c2
Muhammad Taezeem Tariq <taezeemtariq@outlook.com>
2026-06-06 23:16:17 +0530

    fix: disable Vercel bodyParser on api/ocr; lower vision resize threshold to 250KB; restore original smooth 30ms stream interval

--- api/ocr.js
+++ api/ocr.js
@@ -3,3 +3,9 @@ const { handleOcrRequest } = require('./_lib/spectrix');
 module.exports = async function handler(req, res) {
   return handleOcrRequest(req, res);
 };
+
+module.exports.config = {
+  api: {
+    bodyParser: false
+  }
+};
